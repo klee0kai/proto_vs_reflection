09dfa30ae497931d4f82fea3c687a9b85f6db9fe
klee0kai <klee0kai@gmail.com>
2023-09-08 23:15:15 +0400

    normal speed test

--- term/src/main/java/com/example/Main.java
+++ term/src/main/java/com/example/Main.java
@@ -41,7 +41,7 @@ public class Main {
     private static void runTest(Function<SomeCmd, Void> bridge) {
         long startTime = System.currentTimeMillis();
 
-        long testCount = 1000;
+        long testCount = 100_000;
         long testProcessMark = testCount / 10;
 
         for (long i = testCount; i > 0; i--) {
